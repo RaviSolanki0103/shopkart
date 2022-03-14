612ec3dd8163615722f2cf237296a9e1b7cd35bc
RaviSolanki27 <ravisolanki9322@gmail.com>
2022-03-14 19:12:53 +0530

    Added carousel

--- front_end/src/screens/Homepage.jsx
+++ front_end/src/screens/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import CarouselScreen from "../components/carousel/CarouselScreen";
 import Navbar from "../components/navbar/Navbar";
 
 function Homepage() {
   return (
     <div>
       <Navbar />
+      <CarouselScreen/>
     </div>
   );
 }
