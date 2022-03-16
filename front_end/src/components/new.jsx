99f2f032021669576160ada4dede6971c609134e
RaviSolanki27 <ravisolanki9322@gmail.com>
2022-03-16 19:20:22 +0530

    Added specific product page

--- front_end/src/components/new.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const New=()=> {
-  return (
-    <div>this is a new </div>
-  )
-}
-
-export default New
--- front_end/src/router/Routing.jsx
+++ front_end/src/router/Routing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Category from "../screens/Category";
 import Homepage from "../screens/Homepage";
+import SpecificProduct from "../screens/SpecificProduct";
 
 function Routing() {
   return (
@@ -9,6 +10,7 @@ function Routing() {
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/product-category" element={<Category />} />
+        <Route exact path="/product" element={<SpecificProduct />} />
         <Route exact path="*" element={<Homepage />} />
       </Routes>
     </div>
