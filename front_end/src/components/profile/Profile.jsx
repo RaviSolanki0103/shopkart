0f837df9d1a68fa81b2abc2db957e7dc04c1cdea
priyaITPath <priyap.itpath@gmail.com>
2022-03-23 16:40:20 +0530

    Add css in profile and Manage Address module

--- front_end/src/components/profile/Profile.jsx
+++ front_end/src/components/profile/Profile.jsx
@@ -11,75 +11,53 @@ import {
 const { Sider, Content } = Layout;
 
 function Profile() {
-   
+
     return (
         <>
             <Layout className="profile-page-layout">
-                <Sider theme="light" className="profile-sidebar">
-
-                    <Card size="medium" title="My Profile" >
-                        <a class="sidebar-nav-link" href="/profile">
-                            <p><ProfileTwoTone />  Profile Information</p>
-                        </a>
-                    </Card>
-                    <Card size="medium" >
-                        <a class="sidebar-nav-link"  href="/address">
-                            <p> <DatabaseFilled /> Manage Address</p>
-                        </a>
-                    </Card>
-                    <Card size="medium" >
-                        <Row>
-                            <Col>
-                                <a className="sidebar-nav-link" href="/order">
-                                    <p> <ShoppingOutlined /> My Orders</p>
-                                </a>
-                            </Col>
-                        </Row>
-                    </Card>
-                    <Card size="medium" >
-                        <Row>
-                            <Col>
-                                <a class="sidebar-nav-link" href="#0">
-                                    <p> <HeartFilled /> My Wishlist</p>
-                                </a>
-                            </Col>
-                        </Row>
-                    </Card>
-                    <Card size="medium" >
-                        <Row>
-                            <Col>
-                                <a class="sidebar-nav-link" href="#0">
-                                    <p> <PoweroffOutlined /> Logout</p>
-                                </a>
-                            </Col>
-                        </Row>
-                    </Card>
+                <Sider className="sidebar" style={{ background: "white" }} width={270}>
+                <Card  title="My Profile"/>  
+                <Card className="Profile-card" >
+                <a className="Profile-card-link" href="/profile"><ProfileTwoTone /> Personal Information</a>
+                </Card>
+                <Card className="Profile-card">
+                <a className="Profile-card-link" href="/address"><DatabaseFilled /> Manage Address</a>
+                </Card>
+                <Card className="Profile-card">
+                <a className="Profile-card-link" href="/order"><ShoppingOutlined /> My Order</a>
+                </Card>
+                <Card className="Profile-card">
+                <a className="Profile-card-link" href="/wishlist"><HeartFilled /> My Wishlist</a>
+                </Card>
+               <Card className="Profile-card">
+               <a className="Profile-card-link" ><PoweroffOutlined /> Logout</a>
+               </Card>
                 </Sider>
                 <Content>
-                <div className="margin-10-px order-list-of-card">
-                <Card>
-                    <div className="main-content" >
-                        <div className="main-title">
-                            <h4 >Personal Information <button className="editbtn">Edit</button></h4>
-                        </div>
-                        <div className="infoform">
-                            <form className="form">
-                                <label>Name</label><br /><br />
-                                <input type="text" placeholder="Priya" readonly="true"/>  <input type="text" placeholder="Patel"  readonly="true"/> <br /><br />
-                                <label>Gender</label><br /><br />
-                                <input type="radio" name="gender" value="male"  />
-                                <label> Male </label>
-                                <input type="radio" name="gender" value="female" checked  />
-                                <label> Female</label><br /><br />
-                                <label>Email Address</label><br /><br />
-                                <input type="email" name="email" placeholder="priya@gmail.com"  readonly="true"/><br /><br />
-                                <label >Phone Number</label><br /><br />
-                                <input type="number" name="phone" placeholder="1234567890"  readonly="true"/>
-                            </form>
-                        </div>
+                    <div className="margin-10-px order-list-of-card">
+                        <Card>
+                            <div className="main-content" >
+                                <div className="main-title">
+                                    <h4 >Personal Information <button className="editbtn">Edit</button></h4>
+                                </div>
+                                <div className="infoform">
+                                    <form className="form">
+                                        <label className="form-input-label">Name</label><br /><br />
+                                        <input className="form-input" type="text" placeholder="Priya" readonly="true" />  <input className="form-input" type="text" placeholder="Patel" readonly="true" /> <br /><br />
+                                        <label className="form-input-label">Gender</label><br /><br />
+                                        <input className="form-input-radio-btn" type="radio" name="gender" value="male" />
+                                        <label className="form-input-radio"> Male </label>
+                                        <input className="form-input-radio-btn" type="radio" name="gender" value="female" checked />
+                                        <label className="form-input-radio"> Female</label><br /><br />
+                                        <label className="form-input-label">Email Address</label><br /><br />
+                                        <input className="form-input" type="email" name="email" placeholder="priya@gmail.com" readonly="true" /><br /><br />
+                                        <label className="form-input-label">Phone Number</label><br /><br />
+                                        <input className="form-input" type="number" name="phone" placeholder="1234567890" readonly="true" />
+                                    </form>
+                                </div>
+                            </div>
+                        </Card>
                     </div>
-                    </Card>
-              </div>
                 </Content>
             </Layout>
 
