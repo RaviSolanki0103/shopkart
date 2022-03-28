8040e17a48220ba19899cf44102aa2b183628558
priyaITPath <priyap.itpath@gmail.com>
2022-03-28 14:49:08 +0530

    changes in userschema

--- back_end/models/userSchema.js
+++ back_end/models/userSchema.js
@@ -3,32 +3,71 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    fname: {
-        type: 'String',
-        required: true,
-        minlength: 3,
-    },
-    lname: {
-        type: 'String',
-        required: true,
-        minlength: 3,
-    },
-    gender:{
-        type: 'String',
-        enum: ["male", "female"],
-        required: true,
-    },
+  fname: {
+    type: 'String',
+    required: true,
+    minlength: 3,
+  },
+  lname: {
+    type: 'String',
+    required: true,
+    minlength: 3,
+  },
+  gender: {
+    type: 'String',
+    enum: ["male", "female"],
+    required: true,
+  },
   email: {
     type: String,
     required: true,
+    unique: [true, "Email id already present!!"],
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email!!")
+      }
+    }
   },
   phone: {
     type: Number,
     required: true,
+    min: 10,
+    unique: true
   },
   password: {
     type: String,
     required: true,
+  },
+  addresses: {
+    type: [{
+      _id: false,
+      name: {
+        type: 'String',
+        required: true,
+      },
+      email: {
+        type: 'String',
+        required: true,
+        validate(value) {
+          if (!validator.isEmail(value)) {
+            throw new Error("Invalid email")
+          }
+        }
+      },
+      mobile: {
+        type: Number,
+        required: true,
+        min: 10,
+      },
+      address: {
+        type: 'String',
+        required: true,
+      },
+      pincode: {
+        type: 'Number',
+        required: true,
+      }
+    }],
   },
   tokens: [
     {
@@ -57,15 +96,15 @@ userSchema.pre("save", async function (next) {
 });
 
 // We are generating token
-userSchema.methods.generateAuthToken = async function() {
-   try {
-     let mytoken = jwt.sign({_id:this._id}, process.env.SECRET_KEY);
-     this.tokens = this.tokens.concat({ token: mytoken});
-     await this.save();
-     return mytoken;
-   } catch (error) {
-     console.log(error);
-   }
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let mytoken = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: mytoken });
+    await this.save();
+    return mytoken;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const User = mongoose.model("USER", userSchema); // USER is collection name in database
